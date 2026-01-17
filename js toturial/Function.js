59b8756300905508df548316a2a889bb2922b20d
Shaikh Matin <shaikhmatin723@gmail.com>
2026-01-17 19:08:48 +0530

    add Function in js practice

---
+++ js toturial/Function.js
@@ -0,0 +1,27 @@
+function nice(name){
+    console.log("hey "+ name + " you are good")
+    console.log("hey "+ name + " you are nice")
+    console.log("hey "+ name +" your t-shirt is nice ")
+     console.log("hey "+ name +" you are nice person")
+
+}
+nice("Matin")
+nice ("ritesh")
+
+
+function sum (a,b){
+    // console.log( a + b)
+    return a + b
+}
+result = sum(3, 5)
+console.log(" the sum of these number is ", result)
+
+
+
+
+
+
+const func1 = ()=>{
+    console.log("i am arrow function",x)
+}
+func1(34);
