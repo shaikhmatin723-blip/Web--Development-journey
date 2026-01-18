9833da156e5b3ae210703f4e0f0f6395dbd6911a
Shaikh Matin <shaikhmatin723@gmail.com>
2026-01-18 10:46:39 +0530

    add string in js practice

---
+++ js toturial/string.js
@@ -0,0 +1,29 @@
+
+// String in Single
+console.log(" this is string info")
+let a= " matin";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+
+// String Calculation Lenght
+
+console.log(a.length);
+
+
+
+// Combaine two string 
+let name ="Matin"
+let friend =" Ritesh"
+console.log("his name is " + name + "And his friend" + friend)
+console.log('His Name is ${name} His friend name is ${Ritesh}')
+
+
+
+
+let b ="Shaikh"
+console.log(b.toUpperCase)
+console.log(b.toLowerCase)
+console.log(b.slice(1,5))
+console.log(b.replace("sh","77"))
+console.log(b.concat(a, "PRATIK"))
